f3f7220c9cfeff65013342ebcd1e5b5dda064844
Prashant sagar <prashant.sagar73@gmail.com>
2021-05-30 02:50:01 +0530

    react app with header and footer

--- react-drf/blogapi/src/index.js
+++ react-drf/blogapi/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as serviceWorker from './serviceWorker';import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing =(
+    <Router>
+		<React.StrictMode>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={App} />
+			</Switch>
+			<Footer />
+		</React.StrictMode>
+	</Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(routing, document.getElementById('root'));
+serviceWorker.unregister();
